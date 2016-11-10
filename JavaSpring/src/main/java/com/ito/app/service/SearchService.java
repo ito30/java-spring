8ac9f91131b9dfeaab91b0ba986b243105ec0d4b
Marshall Harry <marshall@tiket.com>
2016-11-10 11:16:50 +0700

    tambahin log, save session, sama sabre cmd

--- JavaSpring/src/main/java/com/ito/app/service/SearchService.java
+++ JavaSpring/src/main/java/com/ito/app/service/SearchService.java
@@ -6,9 +6,11 @@ import org.springframework.stereotype.Service;
 
 import com.ito.app.dto.search.Search;
 import com.ito.bean.AbacusAvailability;
+import com.ito.out.SabreCommandLLSExecution;
 import com.ito.out.search.BargainFinderMaxExecution;
 import com.ito.out.session.SessionCreateExecution;
 import com.snail.core.beans.DeliveryMap;
+import com.snail.core.util.DateUtil;
 
 @Service
 public class SearchService {
@@ -20,14 +22,44 @@ public class SearchService {
 	}
 
 	public DeliveryMap search(Search search) {
+		
+		String debugPath = "snail_logs/abacus/"+DateUtil.currentUnixTimestamp() + "_" 
+				+ this.getClass().getSimpleName();
+		
+		SabreCommandLLSExecution cmd1 = new SabreCommandLLSExecution("OVHE/ROUT*");
+		cmd1.run(debugPath, session);
+		SabreCommandLLSExecution cmd2 = new SabreCommandLLSExecution("OVHE/ROUT/RAVL/HCC-CERT");
+		cmd2.run(debugPath, session);
+		SabreCommandLLSExecution cmd3 = new SabreCommandLLSExecution("OVHE/ROUT/RPNR/HCC-CERT");
+		cmd3.run(debugPath, session);
+		SabreCommandLLSExecution cmd4 = new SabreCommandLLSExecution("OVHE/ROUT/SPNR/HCC-CERT");
+		cmd4.run(debugPath, session);
+		SabreCommandLLSExecution cmd5 = new SabreCommandLLSExecution("OVHE/ROUT/RETP/HCC-CERT");
+		cmd5.run(debugPath, session);
+		SabreCommandLLSExecution cmd6 = new SabreCommandLLSExecution("OVHE/ROUT/RETK/HCC-CERT");
+		cmd6.run(debugPath, session);
+		SabreCommandLLSExecution cmd7 = new SabreCommandLLSExecution("OVHE/ROUT/CPNR/HCC-CERT");
+		cmd7.run(debugPath, session);
+		SabreCommandLLSExecution cmd8 = new SabreCommandLLSExecution("TTY-ON/SEND");
+		cmd8.run(debugPath, session);
+		
 		BargainFinderMaxExecution bfm = new BargainFinderMaxExecution(
 				search, session.getConfig().getIpcc());
-		bfm.run(session);
+		bfm.run(debugPath, session);
 		
 		DeliveryMap result = new DeliveryMap();	
-		List<AbacusAvailability> listAv = bfm.getListAv();
-		result.put("go", listAv);
-		result.put("count", listAv.size());
+		if (search.isRoundTrip()) 
+		{
+			List<DeliveryMap> results = bfm.getResults();
+			result.put("go", results);
+			result.put("count", results.size());
+		}
+		else
+		{
+			List<AbacusAvailability> listAv = bfm.getListAv();
+			result.put("go", listAv);
+			result.put("count", listAv.size());
+		}	
 		
 		return result;
 	}
--- JavaSpring/src/main/java/com/ito/out/AbstractAbacusExecution.java
+++ JavaSpring/src/main/java/com/ito/out/AbstractAbacusExecution.java
@@ -22,11 +22,15 @@ import com.ito.common.HttpManager;
 import com.ito.common.SoapHandler;
 import com.ito.fault.AbacusFaultManager;
 import com.ito.out.session.SessionCreateExecution;
+import com.snail.core.util.FileUtil;
+import com.snail.core.util.SoapDebugUtil;
 import com.snail.core.util.SoapUtil;
 
 public abstract class AbstractAbacusExecution {
 	
-	public void run(SessionCreateExecution _session) {
+	protected String debugPath;
+	
+	public void run(String debugPath, SessionCreateExecution _session) {
 		CloseableHttpResponse response = null;
 		HttpRequestBase httpRequest = null;
 		try {
@@ -43,16 +47,20 @@ public abstract class AbstractAbacusExecution {
 			
 			httpRequest = soapRequest.generate();
 			
+			printResult(debugPath,soapRequest.getMessage(), "_request.xml");
+			
 			response = HttpManager.getInstance().run(
 					httpRequest, new BasicCookieStore());
 
 			HttpEntity entity = response.getEntity();
 			
-			String string = IOUtils.toString(entity.getContent());
+			String string = IOUtils.toString(entity.getContent());			
 
 			SOAPMessage soapResult = SoapUtil.toSoapMessage(
 					SOAPConstants.SOAP_1_1_PROTOCOL, string);
 
+			printResult(debugPath,soapResult, "_response.xml");
+			
 			// check fault
 			SOAPBody body = soapResult.getSOAPBody();
 			SOAPFault fault = body.getFault();
@@ -153,11 +161,12 @@ public abstract class AbstractAbacusExecution {
 				}
 				throw AbacusFaultManager.getInstance().soapPenetration(message, this, "");
 			}
+			_session.saveSession(session);
 										
 		} catch (Exception e) {
 			
 		} finally {			
-			onCleaning(httpRequest, response);
+			onCleaning(httpRequest, response);			
 		}
 	}
 		
@@ -177,6 +186,12 @@ public abstract class AbstractAbacusExecution {
 				// do nothing
 			}
 		}
+	}
+	
+	public void printResult(String path,SOAPMessage soapMessage, String status){
+		
+		FileUtil.createDirectory(path);
+		SoapDebugUtil.writeToFile(soapMessage, path+"/"+System.currentTimeMillis()+"_"+this.getClass().getSimpleName() + status);
 	}
 	
 	private boolean isExists(String input, List<String> list)
