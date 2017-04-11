e89cb3bdaed2404383ca7185ef3c81f23030fe12
Marshall Harry <m.harry270891@gmail.com>
2017-04-11 16:42:02 +0700

    ga tau ini apa commit aja ya hahaha

--- JavaSpring/src/main/java/com/ito/app/execution/BargainFinderMaxExecution.java
+++ JavaSpring/src/main/java/com/ito/app/execution/BargainFinderMaxExecution.java
@@ -16,6 +16,9 @@ import com.snail.core.beans.DeliveryMap;
 import com.snail.core.beans.TFlightSelect;
 import com.snail.core.util.SoapHandler;
 
+import rx.Observable;
+import rx.schedulers.Schedulers;
+
 public class BargainFinderMaxExecution extends AbstractAbacusExecution{
 
 	private AbacusSearchInquiry inquiry;
@@ -40,53 +43,84 @@ public class BargainFinderMaxExecution extends AbstractAbacusExecution{
 	}
 
 	// ON RUN
+//	@Override
+//	public void onRunDone(SoapHandler result) throws Exception {
+//		listSoapJourney = result.findAsList("PricedItinerary");
+//		listAv = new ArrayList<AbacusAvailability>();
+//		
+//		if(listSoapJourney.size()>0){
+//			Map<String, String> paxMap = new HashMap<String, String>();
+//			paxMap.put("adultCount", Integer.toString(inquiry.getAdult()));
+//			paxMap.put("childCount", Integer.toString(inquiry.getChild()));
+//			paxMap.put("infantCount", Integer.toString(inquiry.getInfant()));
+//			for (SoapHandler soapJourney : listSoapJourney) {
+//				AbacusAvailability aa = AbacusAvailability.create(soapJourney, paxMap, inquiry.getUpsellings());
+//				if( !aa.isMarkAsRemoved() ){
+//					listAv.add(aa);
+//					String arrival = inquiry.getArrival();
+//					if(inquiry.isRoundTrip()){
+//						Upselling upSelling = new Upselling();
+//						for (Upselling _upSelling : inquiry.getUpsellings()) {
+//							if (_upSelling.getAirline().equals(aa.getValidatingCarrier())) {
+//								upSelling = _upSelling;
+//								break;
+//							}
+//						}
+//						Upselling returnUpselling = new Upselling();
+//						for (Upselling _upselling : inquiry.getReturnUpsellings()) {
+//							if (_upselling.getAirline().equals(aa.getValidatingCarrier())) {
+//								returnUpselling = _upselling;
+//								break;
+//							}
+//						}
+//						AbacusAvailability dep = AbacusAvailability.getDepartAvailability(aa, arrival, 0.5, upSelling);
+//						AbacusAvailability ret = AbacusAvailability.getReturnAvailability(aa, arrival, 0.5, returnUpselling);
+//						if( dep == null || ret == null )
+//							continue;
+//						List<AbacusAvailability> avList = new ArrayList<AbacusAvailability>();
+//						avList.add(dep);
+//						avList.add(ret);
+//						AbacusTrip trip = AbacusTrip.createFromList(avList);
+//						DeliveryMap map = new DeliveryMap();
+//						map.put("depart", dep);
+//						map.put("return", ret);
+//						map.put("trip", trip);
+//						results.add(map);
+//					}
+//				}
+//			}
+//		}
+//	}	
+	
 	@Override
 	public void onRunDone(SoapHandler result) throws Exception {
 		listSoapJourney = result.findAsList("PricedItinerary");
 		listAv = new ArrayList<AbacusAvailability>();
-		if(listSoapJourney.size()>0){
-			Map<String, String> paxMap = new HashMap<String, String>();
-			paxMap.put("adultCount", Integer.toString(inquiry.getAdult()));
-			paxMap.put("childCount", Integer.toString(inquiry.getChild()));
-			paxMap.put("infantCount", Integer.toString(inquiry.getInfant()));
-			for (SoapHandler soapJourney : listSoapJourney) {
-				AbacusAvailability aa = AbacusAvailability.create(soapJourney, paxMap, inquiry.getUpsellings());
-				if( !aa.isMarkAsRemoved() ){
-					listAv.add(aa);
-					String arrival = inquiry.getArrival();
-					if(inquiry.isRoundTrip()){
-						Upselling upSelling = new Upselling();
-						for (Upselling _upSelling : inquiry.getUpsellings()) {
-							if (_upSelling.getAirline().equals(aa.getValidatingCarrier())) {
-								upSelling = _upSelling;
-								break;
-							}
-						}
-						Upselling returnUpselling = new Upselling();
-						for (Upselling _upselling : inquiry.getReturnUpsellings()) {
-							if (_upselling.getAirline().equals(aa.getValidatingCarrier())) {
-								returnUpselling = _upselling;
-								break;
-							}
-						}
-						AbacusAvailability dep = AbacusAvailability.getDepartAvailability(aa, arrival, 0.5, upSelling);
-						AbacusAvailability ret = AbacusAvailability.getReturnAvailability(aa, arrival, 0.5, returnUpselling);
-						if( dep == null || ret == null )
-							continue;
-						List<AbacusAvailability> avList = new ArrayList<AbacusAvailability>();
-						avList.add(dep);
-						avList.add(ret);
-						AbacusTrip trip = AbacusTrip.createFromList(avList);
-						DeliveryMap map = new DeliveryMap();
-						map.put("depart", dep);
-						map.put("return", ret);
-						map.put("trip", trip);
-						results.add(map);
-					}
-				}
+		
+		Map<String, String> paxMap = new HashMap<String, String>();
+		paxMap.put("adultCount", Integer.toString(inquiry.getAdult()));
+		paxMap.put("childCount", Integer.toString(inquiry.getChild()));
+		paxMap.put("infantCount", Integer.toString(inquiry.getInfant()));
+		
+		Observable.from(listSoapJourney)	
+		.filter((soapJourney) -> {
+			try {
+				return !AbacusAvailability.create(soapJourney, paxMap, inquiry.getUpsellings()).isMarkAsRemoved();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				return false;
 			}
-		}
-	}		
+		})
+		.subscribe((soapJourney) -> {
+			
+			try {
+				AbacusAvailability aa = AbacusAvailability.create(soapJourney, paxMap, inquiry.getUpsellings());
+				listAv.add(aa);
+			} catch (Exception e) {
+				
+			}			
+		});
+	}	
 	
 	public List<DeliveryMap> getResults() {
 		return results;
